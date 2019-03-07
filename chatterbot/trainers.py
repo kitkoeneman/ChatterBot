8c0e093b2a11f7a78375e94a56629670a080722d
kitkoeneman <Christopher.Koeneman.2014@live.rhul.ac.uk>
2019-03-07 11:36:15 +0000

    Update trainers.py

--- chatterbot/trainers.py
+++ chatterbot/trainers.py
@@ -2,6 +2,7 @@ import os
 import sys
 import csv
 import time
+import constants
 from multiprocessing import Pool, Manager
 from dateutil import parser as date_parser
 from chatterbot.conversation import Statement
@@ -168,6 +169,64 @@ class ChatterBotCorpusTrainer(Trainer):
 
                     statement = self.get_preprocessed_statement(statement)
 
+                    previous_statement_text = statement.text
+                    previous_statement_search_text = statement_search_text
+
+                    statements_to_create.append(statement)
+
+            self.chatbot.storage.create_many(statements_to_create)
+
+
+class CustomCorpusTrainer(Trainer):
+    """
+    Based on ChatterBotCorpusTrainer (above) except when this trainer encounters an affirmitive or
+    """
+
+    def train(self, *corpus_paths):
+        from chatterbot.corpus import load_corpus, list_corpus_files
+
+        data_file_paths = []
+
+        # Get the paths to each file the bot will be trained with
+        for corpus_path in corpus_paths:
+            data_file_paths.extend(list_corpus_files(corpus_path))
+
+        for corpus, categories, file_path in load_corpus(*data_file_paths):
+
+            statements_to_create = []
+
+            # Train the chat bot with each statement and response pair
+            for conversation_count, conversation in enumerate(corpus):
+
+                if self.show_training_progress:
+                    utils.print_progress_bar(
+                        'Training ' + str(os.path.basename(file_path)),
+                        conversation_count + 1,
+                        len(corpus)
+                    )
+
+                previous_statement_text = None
+                previous_statement_search_text = ''
+
+                for text in conversation:
+                    statement_search_text = self.chatbot.storage.tagger.get_bigram_pair_string(text)
+
+                    statement = Statement(
+                        text=text,
+                        search_text=statement_search_text,
+                        in_response_to=previous_statement_text,
+                        search_in_response_to=previous_statement_search_text,
+                        conversation='training'
+                    )
+                    statement.add_tags(*categories)
+
+                    if previous_statement_text:
+                        if previous_statement_text.strip('.?!/;:\'\"') in constants.AFFIRMATIVES:
+                            statements_to_create[-2].add_tags('AFF:' + statement.text)
+                        elif previous_statement_text.strip('.?!/;:\'\"') in constants.NEGATIVES:
+                            statements_to_create[-2].add_tags('NEG:' + statement.text)
+
+                    statement = self.get_preprocessed_statement(statement)
                     previous_statement_text = statement.text
                     previous_statement_search_text = statement_search_text
 
