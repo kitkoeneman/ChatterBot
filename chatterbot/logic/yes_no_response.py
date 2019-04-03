ed8f20974d9b8120ee14b86dbc949210b1c28020
kitkoeneman <Christopher.Koeneman.2014@live.rhul.ac.uk>
2019-04-03 03:37:53 +0100

    Update yes_no_response.py

--- chatterbot/logic/yes_no_response.py
+++ chatterbot/logic/yes_no_response.py
@@ -31,7 +31,12 @@ class YesNoLogicAdapter(LogicAdapter):
         # Using next() on the generator object gets us the desired Statement object with all needed data.
         incomplete_direct_question = self.chatbot.get_last()
         generator_direct_question = self.chatbot.storage.filter(text=incomplete_direct_question.text)
-        direct_question = next(generator_direct_question)
+
+        direct_question = None
+        try:
+            direct_question = next(generator_direct_question)
+        except StopIteration:
+            pass
 
         response_tag = ''
         response_text = ''
@@ -44,19 +49,27 @@ class YesNoLogicAdapter(LogicAdapter):
                 response_tag = self.get_negative_tag(direct_question)
 
         if response_tag:
-            response_tag = response_tag[4:]
-            generator_response = self.chatbot.storage.filter(text=str(response_tag))
+            generator_response = self.chatbot.storage.filter(text=response_tag)
+            try:
+                answer = next(generator_response)
+                answer.confidence = 1
+                return answer
+            except StopIteration:
+                '''
+                There were occasional instances during testing where storage.filter() would return a generator object
+                with no items in it, causing a StopIteration error. This happened despite there being a statement in the
+                database with a matching text field. The issue was hard to reproduce. This is a workaround.
+                '''
+                desired_stmnt = Statement(text=response_tag)
+                desired_stmnt.confidence = 1
+                return desired_stmnt
 
-        if generator_response:
-            # next() is needed to get text from the generator object.
-            response_text = next(generator_response).text
-            generator_answers = self.chatbot.storage.filter(text=response_text)
-            answer = next(generator_answers)
-            answer.confidence = 1
-            return answer
-
-        direct_question.confidence = 0
-        return direct_question
+        # Many direct questions are only tagged for AFF or NEG response, not both.
+        # This logic adapter should still choose responses for those cases, since BestMatch will always be guessing.
+        # SUGGEST response causes the chatbot to query the suggestion tree and bring up undiscussed topics.
+        default_response = Statement(text='SUGGEST')
+        default_response.confidence = 1
+        return default_response
 
     @staticmethod
     def get_affirmative_tag(statement):
@@ -64,7 +77,7 @@ class YesNoLogicAdapter(LogicAdapter):
 
         for tag in list(tags):
             if tag[:4] == 'AFF:':
-                return tag
+                return tag[4:]
 
         return None
 
@@ -74,6 +87,6 @@ class YesNoLogicAdapter(LogicAdapter):
 
         for tag in list(tags):
             if tag[:4] == 'NEG:':
-                return tag
+                return tag[4:]
 
         return None
